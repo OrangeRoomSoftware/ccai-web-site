7387ec342c81bd69af7824b54127fef5b5086a5a
Phill Kenoyer <phill@ors.bz>
2015-06-21 11:22:24 -0700

    Tweaked menu buttons, added footer, added social links

---
+++ footer.php
@@ -0,0 +1,32 @@
+<!-- WP ORS New Daddy Theme - footer.php -->
+
+          <?php dynamic_sidebar("below-content"); ?>
+        </div>
+      </div>
+    </div>
+    <!-- End Site Body -->
+
+    <footer id="site-footer" class="container">
+      <div class="row">
+        <div class="col-lg-12 col-md-12 col-sm-12">
+          <?php dynamic_sidebar("above-footer"); ?>
+          <?php
+            if ( has_nav_menu('bottom') ) {
+              wp_nav_menu( array(
+                'container' => 'nav',
+                'container_class' => '',
+                'theme_location' => 'bottom',
+                'menu_class' => '',
+                'depth' => 1
+                )
+              );
+            }
+          ?>
+          <?php dynamic_sidebar("below-footer"); ?>
+        </div>
+      </div>
+    </footer>
+
+    <?php wp_footer(); ?>
+  </body>
+</html>
--- header.php
+++ header.php
@@ -1,11 +1,8 @@
 <?php
 /**
- * The Header template for our theme
+ * The Header template for our theme.
  *
  * Displays all of the <head> section and everything up till <div id="main">
- *
- * @package ORS
- * @subpackage New Daddy - ORS
  */
 ?><!DOCTYPE html>
 <html lang="en">
@@ -42,31 +39,35 @@
           <img class="pull-left" id="site-logo" src="/wp-content/themes/ccai/images/header.jpg"/>
         </div>
         <div class="col-lg-7 col-md-7 col-sm-7">
-          <?php dynamic_sidebar("above-header"); ?>
+          <?php dynamic_sidebar('above-header'); ?>
           <hgroup>
-            <h1><a href="<?php echo get_option("home")?>/"><?php bloginfo("name")?></a></h1>
+            <h1><a href="<?php echo get_option('home')?>/"><?php bloginfo('name')?></a></h1>
           </hgroup>
-          <?php dynamic_sidebar("below-header"); ?>
+          <?php dynamic_sidebar('below-header'); ?>
         </div>
         <div class="col-lg-3 col-md-3 col-sm-3" style="text-align:center">
-          <form role="search" method="get" id="searchform" class="searchform" action="http://localhost:36747/">
-					<label class="screen-reader-text" for="s">Search</label>
-					<input type="text" value="" name="s" id="s">
-					<input type="submit" id="searchsubmit" value="Go">
-			</form>
-          <img width="150" src="/wp-content/themes/ccai/images/300logo.jpg"/>
+          <form role="search" method="get" id="searchform" class="searchform" action="/">
+					     <label class="screen-reader-text" for="s">Search</label>
+					     <input type="text" value="" name="s" id="s">
+					     <input type="submit" id="searchsubmit" value="Go">
+			    </form>
+          <div id="logo-links">
+            <a href="http://smile.amazon.com/ch/20-1343468" target="_blank"><img width="50" src="/wp-content/themes/ccai/images/smile.jpg"/></a>
+            <a href="/home/about/join/"><img width="50" src="/wp-content/themes/ccai/images/join.jpg"/></a>
+            <a href="https://www.facebook.com/pages/Capital-City-Arts-Initiative/96391381287" target="_blank"><img width="50" src="/wp-content/themes/ccai/images/facebook.jpg"/></a>
+          </div>
         </div>
       </div>
     <!-- Start WordPress Menu -->
     <?php
-    if ( has_nav_menu('top') ) {
-      wp_nav_menu( array(
+    if (has_nav_menu('top')) {
+        wp_nav_menu(array(
         'container' => 'nav',
         'container_class' => 'navbar',
         'theme_location' => 'top',
         'menu_class' => 'nav navbar-nav',
         'walker' => new wp_bootstrap_navwalker(),
-        ) );
+        ));
     }
     ?>
     <!-- End WordPress Menu -->
@@ -81,23 +82,26 @@
         <?php
         $has_sidebar = false;
         $col_width = 'col-lg-12 col-md-12 col-sm-12';
-        if ( has_nav_menu('sidebar') or is_active_sidebar( 'sidebar' ) ) {
-          $has_sidebar = true;
-          $col_width = 'col-lg-9 col-md-9 col-sm-9';
+        if (has_nav_menu('sidebar') or is_active_sidebar('sidebar')) {
+            $has_sidebar = true;
+            $col_width = 'col-lg-9 col-md-9 col-sm-9';
         }
 
-        if ( $has_sidebar )
-          echo '<ul class="sidebar col-lg-3 col-md-3 col-sm-3">';
+        if ($has_sidebar) {
+            echo '<ul class="sidebar col-lg-3 col-md-3 col-sm-3">';
+        }
 
-        if ( has_nav_menu('sidebar') )
-          wp_nav_menu( array( 'theme_location' => 'sidebar', 'container' => 'nav') );
+        if (has_nav_menu('sidebar')) {
+            wp_nav_menu(array('theme_location' => 'sidebar', 'container' => 'nav'));
+        }
 
-        dynamic_sidebar("sidebar");
+        dynamic_sidebar('sidebar');
 
-        if ( $has_sidebar )
-          echo '</ul>';
+        if ($has_sidebar) {
+            echo '</ul>';
+        }
         ?>
         <!-- End Sidebar -->
 
         <div class="<?php echo $col_width ?>">
-          <?php dynamic_sidebar("above-content"); ?>
+          <?php dynamic_sidebar('above-content'); ?>
